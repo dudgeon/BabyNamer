801042869d943ca68844403127155224930269bf
Geoff Dudgeon <dudgeon@gmail.com>
2015-10-07 00:22:11 -0700

    - added InfoViewController (for the "did you know?" screen)

---
+++ BabyNamer/InfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InfoViewController.swift
+//  BabyNamer
+//
+//  Created by Geoffrey Dudgeon on 10/6/15.
+//  Copyright © 2015 Geoff Dudgeon. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    // MODEL
+    
+    // VIEW
+    
+    // CONTROLLER
+    @IBAction func gameOn(sender: AnyObject) {
+        
+        performSegueWithIdentifier("GameOn", sender: self)
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
